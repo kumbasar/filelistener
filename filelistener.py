83354af0a65caac689db8ce55bb6f8510e2099ea
kumbasar <vkumbasar@outlook.com>
2019-08-20 00:31:07 +0300

    open file using with statement
    Signed-off-by: kumbasar <vkumbasar@outlook.com>

--- filelistener.py
+++ filelistener.py
@@ -7,8 +7,6 @@ extensions = ['mp3','pdf','mp4']
 root_dir = '/Users/kumbasar/github/filelistener/'
 
 for extension in extensions:
-    file = open("{}_file.txt".format(extension), "w", encoding = "utf-8")
-    for filename in glob.iglob(root_dir + '**/*.{}'.format(extension), recursive=True):
-        file.write("{}\n".format(os.path.basename(filename)))
-    file.close()
-
+    with open("{}_file.txt".format(extension), "w", encoding = "utf-8") as file: 
+        for filename in glob.iglob(root_dir + '**/*.{}'.format(extension), recursive=True):
+            file.write("{}\n".format(os.path.basename(filename)))
